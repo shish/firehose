e612d620bd590b2b2a4a2d1f1f5ada07583e8707
Shish <shish@shishnet.org>
2013-08-23 13:55:14 +0100

    more efficient protocol (recv)

--- client/firehose/net.py
+++ client/firehose/net.py
@@ -1,13 +1,14 @@
 import socket
 import base64
+import struct
 
 
 class Firehose(object):
     def __init__(self):
         self.sock = None
-        self.__connect()
+        self.connect()
 
-    def __connect(self):
+    def connect(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect(("firehose.shishnet.org", 9988))
 
@@ -16,9 +17,19 @@ class Firehose(object):
         self.sock = None
 
     def get_data(self):
-        return base64.b64decode(self.sock.recv(4096))
+        data_type, = struct.unpack("b", self.sock.recv(1))
+        if data_type == 0:
+            data_len, = struct.unpack(">h", self.sock.recv(2))
+            data = ""
+            while len(data) < data_len:
+                data = data + self.sock.recv(data_len - len(data))
+            return data
+        else:
+            print "Unknown data type: %r" % data_type
+            self.close()
+            self.connect()
 
     def send_data(self, data):
-        self.sock.sendall(base64.b64encode(str(data.data)))
+        self.sock.sendall(data)
 
 
