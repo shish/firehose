853ef78bc1fb5d899f9c141293742b3f998494d4
Shish <shish@shishnet.org>
2013-08-30 17:37:49 +0100

    identity create / export / import

--- client/firehose/gui/mainframe.py
+++ client/firehose/gui/mainframe.py
@@ -3,7 +3,7 @@ import time
 import logging
 
 from firehose.gui.chat import ChatPanel
-from firehose.gui.chums import ChumList
+from firehose.gui.key import CreateKeyDialog
 import firehose.common as common
 
 log = logging.getLogger(__name__)
@@ -11,6 +11,8 @@ log = logging.getLogger(__name__)
 
 myEVT_RECV = wx.NewEventType()
 EVT_RECV = wx.PyEventBinder(myEVT_RECV, 1)
+
+CREATE_NEW = "Create New Identity"
 
 
 class RecvEvent(wx.PyCommandEvent):
@@ -22,7 +24,7 @@ class RecvEvent(wx.PyCommandEvent):
         return self._value
 
 
-class MainFrame(wx.Frame, common.FirehoseClient):
+class MainFrame(common.FirehoseClient):
     def __init__(self, parent):
         # wx.Frame.__init__(self)
         common.FirehoseClient.__init__(self)
@@ -32,7 +34,7 @@ class MainFrame(wx.Frame, common.FirehoseClient):
 
         self.__init_gui(parent)
 
-        self.Bind(EVT_RECV, self.on_recv)
+        self.frame.Bind(EVT_RECV, self.on_recv)
         self.start_recv_thread()
 
     ###################################################################
@@ -40,21 +42,21 @@ class MainFrame(wx.Frame, common.FirehoseClient):
     ###################################################################
 
     def __init_gui(self, parent):
-        wx.Frame.__init__(self, parent, -1, "Firehose Client [%s]" % common.__version__, size=(800, 600))
-        self.Bind(wx.EVT_CLOSE, self.on_win_close)
+        self.frame = wx.Frame(parent, -1, "Firehose Client [%s]" % common.__version__, size=(800, 600))
+        self.frame.Bind(wx.EVT_CLOSE, self.on_win_close)
 
-        self.SetMenuBar(self.__gui_menu())
-        self.statusbar = self.CreateStatusBar()
+        self.frame.SetMenuBar(self.__gui_menu())
+        self.statusbar = self.frame.CreateStatusBar()
 
         self.chums = self.__gui_control_panel()
 
-        self.tabs = wx.Notebook(self)
+        self.tabs = wx.Notebook(self.frame)
         box = wx.BoxSizer(wx.HORIZONTAL)
         box.Add(self.chums, 0, wx.EXPAND)
         box.Add(self.tabs, 1, wx.EXPAND)
         #self.SetAutoLayout(True)
-        self.SetSizer(box)
-        self.Layout()
+        self.frame.SetSizer(box)
+        self.frame.Layout()
 
         self.get_chat("Status")
 
@@ -68,7 +70,7 @@ class MainFrame(wx.Frame, common.FirehoseClient):
         #    log.exception("Failed to create tray icon:")
         #    self.icon = None
         if show:
-            self.Show(True)
+            self.frame.Show(True)
 
     def __gui_menu(self):
         menu_bar = wx.MenuBar()
@@ -76,7 +78,7 @@ class MainFrame(wx.Frame, common.FirehoseClient):
         ###############################################################
         menu = wx.Menu()
         m_exit = menu.Append(wx.ID_EXIT, "E&xit\tAlt-X", "Close window and exit program.")
-        self.Bind(wx.EVT_MENU, self.on_close, m_exit)
+        self.frame.Bind(wx.EVT_MENU, self.on_close, m_exit)
         menu_bar.Append(menu, "&File")
 
         ###############################################################
@@ -91,7 +93,7 @@ class MainFrame(wx.Frame, common.FirehoseClient):
                 for chum in self.get_chums():
                     self.chums.set_status(chum, "Checking...")
                     chum.send("PING %d" % int(time.time()))
-        self.Bind(wx.EVT_MENU, on_toggle_status_request, m_status_request)
+        self.frame.Bind(wx.EVT_MENU, on_toggle_status_request, m_status_request)
 
         m_status_broadcast = menu.Append(2021, "Broadcast own status changes", "", kind=wx.ITEM_CHECK)
         if self.config["status_broadcast"]:
@@ -100,7 +102,7 @@ class MainFrame(wx.Frame, common.FirehoseClient):
             self.config["status_broadcast"] = m_status_broadcast.IsChecked()
             if self.config["status_broadcast"]:
                 self.status = self.status
-        self.Bind(wx.EVT_MENU, on_toggle_status_broadcast, m_status_broadcast)
+        self.frame.Bind(wx.EVT_MENU, on_toggle_status_broadcast, m_status_broadcast)
 
         m_status_respond = menu.Append(2022, "Respond to status reqests", "", kind=wx.ITEM_CHECK)
         if self.config["status_respond"]:
@@ -109,7 +111,7 @@ class MainFrame(wx.Frame, common.FirehoseClient):
             self.config["status_respond"] = m_status_respond.IsChecked()
             if self.config["status_respond"]:
                 self.set_status(self.status)
-        self.Bind(wx.EVT_MENU, on_toggle_status_respond, m_status_respond)
+        self.frame.Bind(wx.EVT_MENU, on_toggle_status_respond, m_status_respond)
 
         menu.AppendSeparator()
 
@@ -128,7 +130,7 @@ class MainFrame(wx.Frame, common.FirehoseClient):
             m_accept_anon.Check(True)
         def on_toggle_accept_anon(evt):
             self.config["accept_anon"] = m_accept_anon.IsChecked()
-        self.Bind(wx.EVT_MENU, on_toggle_accept_anon, m_accept_anon)
+        self.frame.Bind(wx.EVT_MENU, on_toggle_accept_anon, m_accept_anon)
 
         #menu.AppendSeparator()
 
@@ -138,14 +140,14 @@ class MainFrame(wx.Frame, common.FirehoseClient):
         #self.m_start_tray = m_start_tray  # event handler needs this object, not just ID?
         #if self.config.settings["start-tray"]:
         #    m_start_tray.Check(True)
-        #self.Bind(wx.EVT_MENU, self.on_toggle_start_tray, m_start_tray)
+        #self.frame.Bind(wx.EVT_MENU, self.on_toggle_start_tray, m_start_tray)
 
         menu_bar.Append(menu, "&Options")
 
         ###############################################################
         menu = wx.Menu()
         m_about = menu.Append(wx.ID_ABOUT, "&About", "Information about this program")
-        self.Bind(wx.EVT_MENU, self.on_about, m_about)
+        self.frame.Bind(wx.EVT_MENU, self.on_about, m_about)
         menu_bar.Append(menu, "&Help")
 
         ###############################################################
@@ -158,22 +160,23 @@ class MainFrame(wx.Frame, common.FirehoseClient):
         box = wx.BoxSizer(wx.VERTICAL)
         box.SetMinSize((250, 200))
 
-        self.identity_box = wx.ComboBox(self, choices=[i.uid for i in self.get_identities()], style=wx.CB_DROPDOWN|wx.CB_READONLY)
+        self.identity_box = wx.ComboBox(self.frame, choices=[i.uid for i in self.get_identities()] + [CREATE_NEW], style=wx.CB_DROPDOWN|wx.CB_READONLY)
         self.identity_box.Select(0)
-        self.Bind(wx.EVT_COMBOBOX, self.on_identity_selected, self.identity_box)
+        self.frame.Bind(wx.EVT_COMBOBOX, self.on_identity_selected, self.identity_box)
 
-        self.status_box = wx.ComboBox(self, choices=["Available", "Busy"], style=wx.CB_DROPDOWN)
+        self.status_box = wx.ComboBox(self.frame, choices=["Available", "Busy"], style=wx.CB_DROPDOWN)
         self.status_box.Select(0)
-        self.Bind(wx.EVT_COMBOBOX, self.on_status_selected, self.status_box)
-        self.Bind(wx.EVT_TEXT_ENTER, self.on_status_selected, self.status_box)
+        self.frame.Bind(wx.EVT_COMBOBOX, self.on_status_selected, self.status_box)
+        self.frame.Bind(wx.EVT_TEXT_ENTER, self.on_status_selected, self.status_box)
 
         self.chum_grid = wx.FlexGridSizer(0, 2)
         self.chum_grid.AddGrowableCol(0)
 
-        add_chum = wx.Button(self, -1, "Add Chum")
+        add_chum = wx.Button(self.frame, -1, "Import Chum's Public ID")
+        self.frame.Bind(wx.EVT_BUTTON, self.on_add_chum, add_chum)
 
-        get_key = wx.Button(self, -1, "Show My ID")
-        self.Bind(wx.EVT_BUTTON, self.on_get_key, get_key)
+        get_key = wx.Button(self.frame, -1, "Export my Public ID")
+        self.frame.Bind(wx.EVT_BUTTON, self.on_get_key, get_key)
 
         box.Add(self.identity_box, 0, wx.EXPAND)
         box.Add(self.status_box, 0, wx.EXPAND)
@@ -198,15 +201,15 @@ class MainFrame(wx.Frame, common.FirehoseClient):
         self.chum_list = self.get_chums()
 
         for n, chum in enumerate(self.chum_list):
-            name = wx.Button(self, 4100 + n, chum.name)
-            self.Bind(wx.EVT_BUTTON, self.on_chum_selected, name)
+            name = wx.Button(self.frame, 4100 + n, chum.name)
+            self.frame.Bind(wx.EVT_BUTTON, self.on_chum_selected, name)
             self.chum_grid.Add(name, 1, wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
 
-            button = wx.Button(self, 4200 + n, label=self.statuses.get(chum.uid, "Online?"))
-            self.Bind(wx.EVT_BUTTON, self.on_request_ping, button)
+            button = wx.Button(self.frame, 4200 + n, label=self.statuses.get(chum.uid, "Online?"))
+            self.frame.Bind(wx.EVT_BUTTON, self.on_request_ping, button)
             self.chum_grid.Add(button)
 
-        self.Layout()
+        self.frame.Layout()
 
     def set_status(self, chum, state):
         self.statuses[chum.uid] = state
@@ -225,19 +228,50 @@ class MainFrame(wx.Frame, common.FirehoseClient):
     ###################################################################
 
     def on_identity_selected(self, evt):
-        self.set_identity(self.get_chum(self.identity_box.GetValue()))
+        name = self.identity_box.GetValue()
+        if name == CREATE_NEW:
+            dlg = CreateKeyDialog(self.frame)
+            if dlg.ShowModal() == wx.ID_OK:
+                name, comment, email = dlg.GetValue()
+
+                input_data = self.gpg.gen_key_input(name_real=name, name_comment=comment, name_email=email)
+                genkey = self.gpg.gen_key(input_data)
+
+                # TODO: turn genkey into a Key object so we can just do key["uids"][0]
+                self.identity = self.get_chum("%s (%s) <%s>" % (name, comment, email))
+                self.identity_box.Insert(self.identity.name, 0)
+                self.identity_box.Select(0)
+                self.update()
+            else:
+                # revert to old value
+                self.identity_box.SetValue(self.identity.name)
+        else:
+            self.identity = self.get_chum(name)
 
     def on_status_selected(self, evt):
         self.status = self.status_box.GetValue()
 
+    def on_add_chum(self, evt):
+        dlg = wx.FileDialog(self.frame, "Select Chum's Key", wildcard="*.pub", style=wx.FD_OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            path = dlg.GetPath()
+            key_data = file(path).read()
+            self.gpg.import_keys(key_data)
+
     def on_get_key(self, evt):
+        key_data = None
         my_key = self.get_my_key()
         if my_key["keyid"]:
             key_data = self.gpg.export_keys(my_key["keyid"])
-            if key_data:
-                self.get_chat("Status").show("ID for %s:\n%s" % (my_key["uids"][0], key_data))
+
+        if key_data:
+            dlg = wx.FileDialog(self.frame, "Save Public Key As", wildcard="*.pub", style=wx.FD_SAVE)
+            if dlg.ShowModal() == wx.ID_OK:
+                path = dlg.GetPath()
+                file(path, "w").write(key_data)
         else:
-            self.get_chat("Status").show("The currently selected ID has no key")
+            dlg = wx.MessageDialog(self.frame, "Currently selected ID (%s) has no public key" % self.identity.name, "Error", wx.OK|wx.ICON_ERROR)
+            dlg.ShowModal()
 
     def on_chum_selected(self, evt):
         uid = evt.GetId() - 4100
@@ -252,7 +286,7 @@ class MainFrame(wx.Frame, common.FirehoseClient):
         self.save_config()
         #if self.icon:
         #    self.icon.Destroy()
-        self.Destroy()
+        self.frame.Destroy()
 
     def on_about(self, evt):
         info = wx.AboutDialogInfo()
