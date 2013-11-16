e0ea7d547fff7a9d1970628544956ceca128afdb
Shish <shish@shishnet.org>
2013-11-16 11:00:22 +0000

    zeromq makes things nicer

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
 import zmq
 
 context = zmq.Context()
-socket_rep = context.socket(zmq.REP)
-socket_rep.bind("tcp://*:9990")
+socket_pull = context.socket(zmq.PULL)
+socket_pull.bind("tcp://*:9990")
 socket_pub = context.socket(zmq.PUB)
 socket_pub.bind("tcp://*:9989")
 
 try:
     while True:
-        data = socket_rep.recv()
-        print ".",
+        data = socket_pull.recv()
         socket_pub.send(data)
 except KeyboardInterrupt:
-    socket.close()
+    socket_pub.close()
+    socket_pull.close()
     context.term()
